8dac45e07ddb3d35299a6df43e07de369cca1be6
Dmitry Kalyukov <dmitrykalyukov@gmail.com>
2021-11-20 19:48:21 +0200

    Chapter 2: Degenerate Objects

--- money/test.py
+++ money/test.py
@@ -4,5 +4,10 @@ from money import Dollar
 class TestMoney:
     def test_multiplication(self) -> None:
         five = Dollar(5)
-        five.times(2)
-        assert five.amount == 10
+        product = five.times(2)
+
+        assert product.amount == 10
+
+        product = five.times(3)
+
+        assert product.amount == 15
