13dd57cf3fec0d8f9301ec57e7b8ac26c2941d53
Dmitry Kalyukov <dmitrykalyukov@gmail.com>
2021-11-21 11:17:53 +0200

    Chapter 6: Equality for All, Redux

--- money/money.py
+++ money/money.py
@@ -1,20 +1,16 @@
-class Dollar:
+class Money:
     def __init__(self, amount: int) -> None:
         self._amount: int = amount
 
+    def __eq__(self, money: 'Money') -> bool:
+        return self._amount == money._amount
+
+
+class Dollar(Money):
     def times(self, multiplier: int) -> 'Dollar':
         return Dollar(self._amount * multiplier)
 
-    def __eq__(self, dollar: 'Dollar') -> bool:
-        return self._amount == dollar._amount
 
-
-class Franc:
-    def __init__(self, amount: int) -> None:
-        self._amount: int = amount
-
+class Franc(Money):
     def times(self, multiplier: int) -> 'Franc':
         return Franc(self._amount * multiplier)
-
-    def __eq__(self, Franc: 'Franc') -> bool:
-        return self._amount == Franc._amount
--- money/test.py
+++ money/test.py
@@ -11,6 +11,8 @@ class TestMoney:
     def test_equality(self) -> None:
         assert Dollar(5) == Dollar(5)
         assert Dollar(5) != Dollar(6)
+        assert Franc(5) == Franc(5)
+        assert Franc(5) != Franc(6)
 
     def test_franc_multiplication(self) -> None:
         five = Franc(5)
