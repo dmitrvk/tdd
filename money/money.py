01bcfc9451e6b6090dc3bf03e1e1491c22fe89a6
Dmitry Kalyukov <dmitrykalyukov@gmail.com>
2021-11-21 10:40:06 +0200

    Chapter 5: Franc-ly Speaking

--- money/money.py
+++ money/money.py
@@ -7,3 +7,14 @@ class Dollar:
 
     def __eq__(self, dollar: 'Dollar') -> bool:
         return self._amount == dollar._amount
+
+
+class Franc:
+    def __init__(self, amount: int) -> None:
+        self._amount: int = amount
+
+    def times(self, multiplier: int) -> 'Franc':
+        return Franc(self._amount * multiplier)
+
+    def __eq__(self, Franc: 'Franc') -> bool:
+        return self._amount == Franc._amount
--- money/test.py
+++ money/test.py
@@ -1,4 +1,4 @@
-from .money import Dollar
+from .money import Dollar, Franc
 
 
 class TestMoney:
@@ -11,3 +11,9 @@ class TestMoney:
     def test_equality(self) -> None:
         assert Dollar(5) == Dollar(5)
         assert Dollar(5) != Dollar(6)
+
+    def test_franc_multiplication(self) -> None:
+        five = Franc(5)
+
+        assert five.times(2) == Franc(10)
+        assert five.times(3) == Franc(15)
