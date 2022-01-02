64a3c21bccc138fffd11214fa6f5df57a92cc78a
Dmitry Kalyukov <dmitrykalyukov@gmail.com>
2022-01-02 09:52:11 +0300

    Fibonacci - Generalize

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -2,7 +2,7 @@ def fibonacci(number: int) -> int:
     if number == 0:
         return 0
 
-    if number <= 2:
+    if number <= 1:
         return 1
 
-    return 2
+    return fibonacci(number - 1) + fibonacci(number - 2)
