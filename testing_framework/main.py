8be0a92e0686a7cc85a908dd2dc31b91d011360f
Dmitry Kalyukov <dmitrykalyukov@gmail.com>
2022-01-07 14:55:11 +0300

    Chapter 21: Counting

--- testing_framework/main.py
+++ testing_framework/main.py
@@ -1,3 +1,14 @@
+class TestResult:
+    def __init__(self) -> None:
+        self.run_count: int = 0
+
+    def test_started(self) -> None:
+        self.run_count += 1
+
+    def summary(self) -> str:
+        return f'{self.run_count} run, 0 failed'
+
+
 class TestCase:
     def __init__(self, name: str) -> None:
         self.name: str = name
@@ -5,10 +16,15 @@ class TestCase:
     def set_up(self) -> None:
         ...
 
-    def run(self) -> None:
+    def run(self) -> TestResult:
+        result = TestResult()
+        result.test_started()
+
         self.set_up()
         getattr(self, self.name)()
         self.tear_down()
+
+        return result
 
     def tear_down(self) -> None:
         ...
--- testing_framework/test.py
+++ testing_framework/test.py
@@ -8,5 +8,12 @@ class TestCaseTest(TestCase):
 
         assert test.log == 'set_up test_method tear_down'
 
+    def test_result(self) -> None:
+        test = WasRun('test_method')
+        result = test.run()
+
+        assert result.summary() == '1 run, 0 failed'
+
 
 TestCaseTest('test_template_method').run()
+TestCaseTest('test_result').run()
